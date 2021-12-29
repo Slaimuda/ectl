184b26369be37a3f01ba846a67add4bc21fdea31
DKZed <DKZed@users.noreply.github.com>
2021-12-29 20:57:45 +0000

    deploy: DKZed/ectl_repo@bbdda6b3cb6caba72f21ac5ad3b462af7f55ddc9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ectl/precache-manifest.0819144d96509d87199ec51510a1b287.js"
+  "/ectl/precache-manifest.d26a827961b8075678351bfd860b151f.js"
 );
 
 self.addEventListener('message', (event) => {
