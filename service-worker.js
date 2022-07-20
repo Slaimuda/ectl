7472cd01273d7722e3efdc1a26b437a536d318f0
thebombzen <thebombzen@users.noreply.github.com>
2022-07-20 02:08:10 +0000

    deploy: DKZed/ectl_repo@7edda64cb9aafedbca3a4789be8bfa109d86a79f

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ectl/precache-manifest.2636da35b6b4d080db695d6d2e44acfa.js"
+  "/ectl/precache-manifest.bef6c214f12997eef4f77a43d21aa5e3.js"
 );
 
 self.addEventListener('message', (event) => {
